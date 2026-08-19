4d81a718f79811ab6670da8a7c7a758488b26847
Oscar Llamuca <oscarandre998@gmail.com>
2026-08-19 14:34:00 -0500

    First commit

---
+++ src/app.http.ts
@@ -0,0 +1,42 @@
+import http from 'http';
+import fs from 'fs';
+
+const server = http.createServer((req, res) => {
+
+   console.log(req.url);
+
+//    res.writeHead(200, { 'Content-type': 'text/html' });
+//    res.write(`<h1>URL ${req.url}</h1>`);
+//    res.end();
+
+//    const data = { name: 'John Doe', age: 30, city: 'New York'};
+//    res.writeHead(200, {'content-type': 'aplication/json' });
+//    res.end( JSON.stringify(data) );
+
+if (req.url === '/' ) {
+    const htmlFile = fs.readFileSync('./public/index.html','utf-8');
+     res.writeHead(200, { 'Content-type': 'text/html' });
+     res.end(htmlFile);
+     return;   
+}
+if( req.url?.endsWith('.js') ){
+res.writeHead(200, { 'content-type': 'application/javascript' });
+} else if( req.url?.endsWith('.css') ){
+res.writeHead(200, { 'content-type': 'text/css' });
+}
+
+try {
+ const responseContent = fs.readFileSync(`./public${ req.url }`, 'utf-8');
+res.end(responseContent);
+} catch (error) {
+     res.writeHead(404, { 'Content-type': 'text/html' });
+    res.end();
+}
+
+
+
+});
+
+server.listen(8080, () => {
+    console.log('Server runnning on port 8080')
+})
